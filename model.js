43e3bec69ef7887828ecc01d3a2ab27967467364
DamonTran108 <58449765+DamonTran108@users.noreply.github.com>
2020-02-11 22:07:13 +0000

    Update model.js

--- model.js
+++ model.js
@@ -1,43 +1,40 @@
-function setup() {
-    //setup code here
-    model = new Model(500);
-    width = windowWidth;
-    height = windowHeight;
-    rectWidth = width / model.getData().length;
-    rectHeight = height / model.getData().length;
-    shuffle = true;
-    reset = false;
-    createCanvas(width, height);
-}
+class Model {
+    constructor(sizeOfData) {
+        this._data = new Array();
 
-function draw() {
-    //drawing code here
-    background(51);
-    var colorInterval = 360 / model.getData().length;
-    for(var i = 0; i < model.getData().length; i++) {
-
-        thisColor = colorInterval * model.getData()[i];
-        colorMode(HSB);
-        fill(thisColor, 100, 100);
-
-        rect(i * rectWidth, height, rectWidth, -(rectHeight * model.getData()[i]));
+        for(var i = 0; i < sizeOfData; i++) {
+            this._data.push(i+1);
+        }
     }
 
-    if(shuffle) {
-        model.shuffleData();
-        shuffle = false;
+    swap(a, b) {
+        var tempValue;
+
+        tempValue = this._data[a];
+        this._data[a] = this._data[b];
+        this._data[b] = tempValue;
     }
 
-    if(reset) {
-        model.resetData();
-        reset = false;
-    }
-}
+    shuffleData() {
+        var currentIndex = this._data.length;
+        var randomIndex;
+        var tempValue;
 
-function windowResized() {
-    width = windowWidth;
-    height = windowHeight;
-    rectWidth = width / model.getData().length;
-    rectHeight = height / model.getData().length;
-    resizeCanvas(width, height);
+        while(currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            this.swap(currentIndex, randomIndex);
+        }
+    }
+
+    resetData() {
+        for(var i = 0; i < this._data.length; i++) {
+            this._data[i] = (i+1);
+        }
+    }
+
+    getData() {
+        return this._data;
+    }
+
 }
