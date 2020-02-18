b6a2d323b9aae2c8642cb9ba1b5c858af915cda1
Jake <jaketurner810@gmail.com>
2020-02-18 22:03:45 +0000

    Nav reuse script added

---
+++ js/utils.js
@@ -0,0 +1,5 @@
+function loadNav(homePath, sortingPath){
+
+    document.write("<nav><ul><li><a href='" + homePath + "index.html'>Home</a></li><li><a href='" + sortingPath + "sorting.html'>Sorting</a></li><li id='dropdown'><a href='#'>Searching</a><div class='dropdown-content'><a href='#'>...</a><a href='#'>...</a><a href='#'>...</a></div></li></ul></nav>")
+    
+}
